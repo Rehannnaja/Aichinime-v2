d98aa99c560ad1eaa9c0b2e60244ff0300fb894b
!Taka dev <kontol543215@gmail.com>
2025-06-23 00:58:55 +0700

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black text-gray-400 text-center py-4 mt-10">
+      <p>Aichinime © {new Date().getFullYear()} - Made with ♥ for anime & manga fans</p>
+    </footer>
+  )
+}
